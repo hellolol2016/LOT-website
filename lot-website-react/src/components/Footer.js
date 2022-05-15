383e916251a5edb8cfc8c2a90cb18e4b686a6f85
ardatasc <ardatasci@outlook.com>
2022-05-15 05:27:06 -0700

    test

---
+++ lot-website-react/src/components/Footer.js
@@ -0,0 +1,31 @@
+import { React } from "react";
+import data from "../pages/data";
+function Footer() {
+  let socials = data.socials[0].links;
+  return (
+    <footer className="darker-background">
+      <h2 className="white-font">
+        Feel free to contact us, we'd love to hear from you!
+      </h2>
+      <ul>
+        {socials.map((social) => {
+          const { icon, url } = social;
+          return (
+            <li key={social.name}>
+              <a
+                className="social-link"
+                rel="noreferrer"
+                target="_blank"
+                href={url}
+              >
+                {icon}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
