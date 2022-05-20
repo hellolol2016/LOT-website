17dcd3cad01249877ab7a04895c02ea64bf0d75d
ardatasc <ardatasci@outlook.com>
2022-05-20 15:36:22 -0700

    ok methinks this is the fix i think

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import {React, useEffect, useRef} from "react"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Donate from "./pages/Donate"
+import Involve from "./pages/Involve"
+import Error from "./pages/Error"
+import Project from "./pages/Project.js"
+import Gallery from "./pages/Gallery"
+import lotIcon from '../src/media/whitecirclelot.png'
+import { Helmet } from "react-helmet"
+
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+
+import anime from "animejs"
+
+function App() {
+  return (
+    <Router>
+      <Helmet>
+        <meta charset="UTF-8" />
+        <meta name="description" content="The LOT Foundation website"/>
+        <meta name="keywords" content="HTML, CSS, JavaScript" />
+        <meta property="og:title" content="Laugh Out Together" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://laughouttogether.org" />
+        <meta property="og:image" content={lotIcon} />
+        <meta property="og:description" content="The LOT Foundation website" />
+        <meta name="theme-color" content="#ff6b6b" />
+
+        <meta name="twitter:card" content="summary_large_image"/>
+        <link rel="icon" href={lotIcon} type="image/x-icon" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        />
+        <title>LOT Foundation</title>
+      </Helmet>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/donate">
+          <Donate />
+        </Route>
+        <Route exact path="/involve">
+          <Involve />
+        </Route>
+        <Route exact path="/gallery">
+          <Gallery />
+        </Route>
+        <Route exact path="/project/:id">
+          <Project />
+        </Route>
+        <Route exact path="*">
+          <Error />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/Project.js
@@ -0,0 +1,150 @@
+import {React, useRef, useEffect} from "react";
+import { useParams, Link } from "react-router-dom";
+import data from "./data";
+import anime from 'animejs'
+import { Helmet } from "react-helmet";
+function Project() {
+
+  const animationRef = useRef(null)
+  useEffect(() => {
+    let tl = anime.timeline({
+      easing: "easeOutExpo",
+      duration: 1000
+    });
+
+    tl.add({
+      targets: "li",
+      translateY: -10,
+      delay: anime.stagger(50, { start: 500 }),
+      direction:"alternate"
+    })
+    
+    animationRef.current = tl;
+
+    let animation = anime({
+      targets:".lot",
+      opacity: 0.8,
+      direction: "alternate",
+      loop: true,
+      easing: "easeInOutSine",
+    })
+
+    animationRef.current = animation.play();
+
+    
+    var buttonEl = document.querySelectorAll("li, img");
+
+    async function animateButton(el, scale, duration, elasticity) {
+      anime.remove(el);
+      animationRef.current = anime({
+        targets: el,
+        scale: scale,
+        duration: duration,
+        elasticity: elasticity,
+      });
+    }
+
+    async function enterButton(el) {
+      animateButton(el, 1.1, 800, 400);
+    }
+
+    async function leaveButton(el) {
+      animateButton(el, 1.0, 600, 300);
+    }
+
+    for (var i = 0; i < buttonEl.length; i++) {
+      buttonEl[i].addEventListener(
+        "mouseenter",
+        async function (e) {
+          enterButton(e.target);
+        },
+        false
+      );
+
+      buttonEl[i].addEventListener(
+        "mouseleave",
+        async function (e) {
+          leaveButton(e.target);
+        },
+        false
+      );
+    }
+  }, [])
+
+
+
+
+  const { id } = useParams();
+  let projects = data.projects;
+  let selected = null;
+  let type, pid, title, text, subtitle, yt, image = "";
+  projects.map((project) => {
+    if(project.pid == id){
+      selected = project;
+      type = project.type;
+      pid = project.pid;
+      title = project.title;
+      text = project.text;
+      subtitle = project.subtitle;
+      yt = project.yt;
+      image = project.image;
+      console.log(image)
+    }
+    });
+    if(type == "vid-art"){
+      console.log("vid-art")
+      return (
+        <>
+          <Helmet>
+            <title>{title}</title>
+          </Helmet>
+          <div class="body1">
+            <h2 class="thick-header">{title}</h2>
+            <div class="row">
+              <div class="one-half">{text}</div>
+              <div class="one-half">
+                <iframe
+                  width="560"
+                  height="315"
+                  src={yt}
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen
+                ></iframe>
+              </div>
+            </div>
+          </div>
+        </>
+      );
+      } else {
+        console.log("img art")
+        return (
+          <>
+            <Helmet>
+              <title>{title}</title>
+            </Helmet>
+            <div class="body1">
+              <h2 class="thick-header">{title}</h2>
+              <div class="row">
+                <div class="one-half">
+                  <img src={image}></img>
+                  <p>{subtitle}</p>
+                </div>
+                <div class="one-half">{text}</div>
+              </div>
+            </div>
+          </>
+        );
+      }
+      console.log(type, pid, title, text, subtitle, yt, image)
+  if(selected == null){
+    return(
+      <>
+        <h1>Sorry, no project avaliable</h1>
+      </>
+    )
+  }
+}
+
+export default Project;
